9858764ba5ea11ae39cd5c29b76cd4bc53433491
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-01 20:35:21 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
-
 
 @Component({
   selector: 'app-root',
@@ -11,20 +9,25 @@ import { CategoryComponent } from './category/category.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  //create variables and functions here
-  title = 'Angular Demo Application'
-  name = 'Uzzma'
-  imageurl = 'https://i.pinimg.com/736x/2b/d4/69/2bd469e044e93137be711cfa36943364.jpg'
+  title = 'Angular Demo Application';
+  name = 'Uzzma';
+  imageurl = 'https://i.pinimg.com/736x/2b/d4/69/2bd469e044e93137be711cfa36943364.jpg';
+  alternateImageUrl = 'https://cdn.shopify.com/s/files/1/2652/9998/files/what_does_single_red_rose_mean.jpg?v=1703849723';
+  isAlternateImage = false;
 
   printButton() {
-    console.log('Button Clicked');
+    const imageElement = document.getElementById('image') as HTMLImageElement;
+    if (imageElement) {
+      if (this.isAlternateImage) {
+        imageElement.src = this.imageurl;
+      } else {
+        imageElement.src = this.alternateImageUrl;
+      }
+      this.isAlternateImage = !this.isAlternateImage;
+    }
   }
 
-  isAvailable = true
-  isNotAvailable = false
-
-
-  Category = 'Stationary'
-
+  isAvailable = true;
+  isNotAvailable = false;
+  Category = 'Stationary';
 }
-
