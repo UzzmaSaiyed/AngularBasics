ee06b7346f491a1820f784034ebc47e99892c4b6
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-04 22:34:50 +0530

    Directive added

--- src/app/pages/template-example/template-example.component.ts
+++ src/app/pages/template-example/template-example.component.ts
@@ -1,17 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-example',
   standalone: true,
-  imports: [],
-  template: `
-    <div>
+  imports: [CommonModule],
+  template:
+    // template 
+    ` <div>
       <h1>Welcome to Angular Template Example</h1>
       <p>This is a simple Angular component using template syntax.</p>
-    </div>`,
+      <ul>
+      <li *ngFor="let place of places">{{place}}</li>
+      </ul>
+    </div>` // directive
+  ,
   // templateUrl: './template-example.component.html',
   styleUrl: './template-example.component.css'
 })
 export class TemplateExampleComponent {
+
+  places: string[] = ["LA", "NY", "NZ"]
 
 }
