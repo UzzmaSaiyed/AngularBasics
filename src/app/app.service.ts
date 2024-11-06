c60c996560d42da890ef7a4a295e005a61cce923
Uzzmasaiyed <writeruzzi@gmail.com>
2024-11-06 23:44:45 +0530

    updated

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -8,7 +8,9 @@ import { Observable } from 'rxjs';
 })
 export class AppService {
   // private _url: string = '/assets/data/product.json'
-  private _url: string = 'http://localhost:3000/api/product'
+  // private _url: string = 'http://localhost:3000/api/product'
+  private _url: string = 'https://product-api-chi-lake.vercel.app/api/product'
+
 
   constructor(private http: HttpClient) {
 
